d8894597d0dda38539bb67cc188ba60af1a72b14
Maksim <status187@yandex.ru>
2021-06-09 11:51:17 +0300

    improvements

--- js/main.js
+++ js/main.js
@@ -4,17 +4,49 @@ let random = Math.round(Math.random(max) * max);
 
 console.log(random);*/
 //Первая функия
-let giveRandomNumber = function (min, max) {
-  (max <= min || min <= 0) ? ('Неверно введены данные') : giveRandomNumber = Math.round(Math.random() * max);
 
-  return giveRandomNumber;
-};
+function giveRandomNumber(min, max) {
+  if (max <= min || min <= 0) {
+    throw new RangeError('Данные некорректны');
+  }
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+
+}
+
+giveRandomNumber(3, 4);
+
 //Вторая функция
-giveRandomNumber(2, 20);
 
+function randomInteger(min, max, fixedFractionValue) {
+  if (max <= min || min <= 0) {
+    throw new RangeError('Данные некорректны');
+  }
+  const rand = min + Math.random() * (max + 1 - min);
+  return Number(rand.toFixed(fixedFractionValue));
+
+}
+
+randomInteger(3, 9, 3);
+/*
 let giveRandomCoordinate = function (min, max) {
   (max <= min || min <= 0) ? ('Неверно введены данные') : giveRandomCoordinate = Math.random() * max;
   return giveRandomCoordinate;
 };
 
 giveRandomCoordinate(2, 100);
+
+*/
+/*
+let random;
+let giveRandomNumber = function (min, max) {
+  if (max <= min || min < 0) {
+    return ('Неверно введены данные');
+  } else {
+    random = Math.round(Math.random() * (max - min)) + min;
+  }
+};
+
+giveRandomNumber(1, 12);
+console.log(random);
+*/
